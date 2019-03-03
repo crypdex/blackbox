7ce3a72be5fc5822f0b800f29b9720c8f116bf76
David Michael <david.michael@gmail.com>
2019-03-03 14:07:25 -0500

    cleanup

--- scripts/generate_encryption_key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-filename=.encryption_key.env
-
-if [[ ! -f ${filename} ]]; then
-    echo "Generating the encryption key."
-    printf "ENCRYPTION_KEY=%s" $(openssl rand -hex 32) >> ${filename}
-else
-    echo "The key has already been generated. Not overwriting."
-fi
