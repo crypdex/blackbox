894a0aa2eca7475174f2b42dcc0d57ce0f705df5
David Michael <david.michael@gmail.com>
2019-04-03 12:32:09 -0400

    just copy the file

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "enabling the systemd service"
-cp /var/lib/blackbox/blackbox.service /etc/systemd/system/blackbox.service
+#cp /var/lib/blackbox/blackbox.service /etc/systemd/system/blackbox.service
 #systemctl enable /etc/systemd/system/blackbox.service
 #systemctl daemon-reload
 #systemctl start blackbox.service
