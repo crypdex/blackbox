ddcd78985885670b0c8ae997b90732420595e16c
David Michael <david.michael@gmail.com>
2019-04-03 12:25:15 -0400

    still testing systemd

--- scripts/postremove.sh
+++ scripts/postremove.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-systemctl stop blackbox.service
-systemctl disable blackbox.service
+#systemctl stop blackbox.service
+#systemctl disable blackbox.service
 rm /etc/systemd/system/blackbox.service
