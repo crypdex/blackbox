8af7b50becfac8c96aad56e642b23f9ab25d7a6c
David Michael <david.michael@gmail.com>
2019-04-03 14:39:51 -0400

    bumping the multiwallet

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,5 +2,5 @@
 echo "enabling the systemd service"
 #cp /var/lib/blackbox/blackbox.service /etc/systemd/system/blackbox.service
 deb-systemd-invoke enable blackbox.service
-deb-systemd-invoke daemon-reload
+deb-systemd-helper daemon-reload
 deb-systemd-invoke start blackbox.service
