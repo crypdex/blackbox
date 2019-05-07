3aff056ffdd7008e3f05659879ecbeff6d0d51b2
David Michael <david.michael@gmail.com>
2019-05-07 16:26:29 -0400

    autogeneration of ip address cert

--- services/sparkswap/generate-id-and-certs.sh
+++ services/sparkswap/generate-id-and-certs.sh
@@ -7,21 +7,94 @@
 
 
 
+EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS}
 
-# Setting this env is ONLY required for a hosted broker setup.
-#
-# This address is used during the build process so that certs can be generated
-# correctly for a hosted (remote) broker daemon.
+SPARKSWAP_DIRECTORY=${SPARKSWAP_DIRECTORY:-~/.sparkswap}
 
-EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS:-localhost}
-
-SPARKSWAP_DIRECTORY=${EXTERNAL_ADDRESS:-~/.sparkswap}
-
-
-
-echo "Creating directories $SPARKSWAP_DIRECTORY and $SPARKSWAP_DIRECTORY/secure"
-
+echo "Creating directories $SPARKSWAP_DIRECTORY/secure"
 mkdir -p ${SPARKSWAP_DIRECTORY}/secure
+
+# RESOLVE THE IP ADDRESS
+
+PREVIOUS_ADDRESS=""
+
+resolve_ip() {
+  echo "Resolving IP address"
+  local file=${SPARKSWAP_DIRECTORY}/ipaddress.txt
+
+  # PREVIOUS
+  if [[ -f ${file} ]]; then
+    while IFS= read line
+    do
+      PREVIOUS_ADDRESS=${line}
+    done <"$file"
+  fi
+
+  # CURRENT
+  if [[ ${EXTERNAL_ADDRESS} == "" ]]; then
+    EXTERNAL_ADDRESS=$(hostname -i)
+  fi
+
+  echo "The current IP is set to ${EXTERNAL_ADDRESS}"
+
+# Write current to file
+/bin/cat <<EOM >${file}
+${EXTERNAL_ADDRESS}
+EOM
+}
+
+# Call the function
+resolve_ip
+
+
+
+#############################################
+# Keypair Generation for SSL to the broker
+#
+# This step creates certs to allow a user to host a broker on a remote machine
+# and have connections to their daemon be secured through ssl
+#
+# Primary use is TLS between Broker-CLI and Broker Daemon
+#
+#############################################
+
+KEY_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-tls.key
+CERT_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-tls.cert
+CSR_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-csr.csr
+
+generate_tls_certs() {
+  echo "Generating TLS certs for Broker Daemon"
+  openssl ecparam -genkey -name prime256v1 > ${KEY_PATH}
+  openssl req -new -sha256 -key ${KEY_PATH} \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+      <(printf "\n[SAN]\nsubjectAltName=DNS:$EXTERNAL_ADDRESS,DNS:localhost")) \
+    -subj "/CN=$EXTERNAL_ADDRESS/O=sparkswap" > ${CSR_PATH}
+  openssl req -x509 -sha256 -key ${KEY_PATH} -in  ${CSR_PATH} -days 36500 \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /etc/ssl/openssl.cnf \
+      <(printf "\n[SAN]\nsubjectAltName=DNS:$EXTERNAL_ADDRESS,DNS:localhost")) > $CERT_PATH
+
+  rm -f ${CSR_PATH}
+}
+
+# If the IP has changed, re-generate the certs
+if [[ ${PREVIOUS_ADDRESS} != ${EXTERNAL_ADDRESS} ]]; then
+  echo "WARNING: IP has changed. Will regenerate TLS certs."
+  generate_tls_certs
+else
+  if [[ -f "$KEY_PATH" ]]; then
+    echo "WARNING: TLS Private Key already exists at $KEY_PATH for Broker Daemon. Skipping cert generation"
+  elif [[ -f "$CERT_PATH" ]]; then
+    echo "WARNING: TLS Cert already exists at $CERT_PATH for Broker Daemon. Skipping cert generation"
+  else
+    generate_tls_certs
+  fi
+fi
+
+
 
 #############################################
 # Keypair Generation of Broker Identity for Relayer
@@ -48,45 +121,4 @@ elif [[ -f "$ID_PUB_KEY" ]]; then
 elif [[ "$NO_IDENTITY" != "true" ]]; then
   openssl ecparam -name prime256v1 -genkey -noout > ${ID_PRIV_KEY}
   openssl ec -in ${ID_PRIV_KEY} -pubout > ${ID_PUB_KEY}
-fi
-
-
-#############################################
-# Keypair Generation for SSL to the broker
-#
-# This step creates certs to allow a user to host a broker on a remote machine
-# and have connections to their daemon be secured through ssl
-#
-# Primary use is TLS between Broker-CLI and Broker Daemon
-#
-#############################################
-echo "Generating the Broker SSL certs"
-
-
-KEY_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-tls.key
-CERT_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-tls.cert
-CSR_PATH=${SPARKSWAP_DIRECTORY}/secure/broker-rpc-csr.csr
-
-
-if [[ -f "$KEY_PATH" ]]; then
-  echo "WARNING: TLS Private Key already exists at $KEY_PATH for Broker Daemon. Skipping cert generation"
-elif [[ -f "$CERT_PATH" ]]; then
-  echo "WARNING: TLS Cert already exists at $CERT_PATH for Broker Daemon. Skipping cert generation"
-else
-  echo "Generating TLS certs for Broker Daemon"
-
-  openssl ecparam -genkey -name prime256v1 > ${KEY_PATH}
-  openssl req -new -sha256 -key ${KEY_PATH} \
-    -reqexts SAN \
-    -extensions SAN \
-    -config <(cat /etc/ssl/openssl.cnf \
-      <(printf "\n[SAN]\nsubjectAltName=DNS:$EXTERNAL_ADDRESS,DNS:localhost")) \
-    -subj "/CN=$EXTERNAL_ADDRESS/O=sparkswap" > ${CSR_PATH}
-  openssl req -x509 -sha256 -key ${KEY_PATH} -in  ${CSR_PATH} -days 36500 \
-    -reqexts SAN \
-    -extensions SAN \
-    -config <(cat /etc/ssl/openssl.cnf \
-      <(printf "\n[SAN]\nsubjectAltName=DNS:$EXTERNAL_ADDRESS,DNS:localhost")) > $CERT_PATH
-
-  rm -f ${CSR_PATH}
 fi
