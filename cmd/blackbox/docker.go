132e706fffbec9066379e7157dc90a015f23bd0c
David Michael <david.michael@gmail.com>
2019-05-20 11:59:07 -0400

    added names and restart policy for docker-compose

--- cmd/blackbox/docker.go
+++ cmd/blackbox/docker.go
@@ -1,9 +1,6 @@
 package blackbox
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/go-cmd/cmd"
 )
 
@@ -128,72 +125,8 @@ func (client *DockerClient) formatServices(flagName string) []string {
 // Swarm-related functions
 // ---------------------------
 
-func (client *DockerClient) EnsureSwarmMode() error {
-	if !client.config.ForceSwarm() {
-		trace("[swarm] flag not set.")
-		return nil
-	}
-
-	trace("[swarm] found swarm setting.")
-	isManager, err := client.IsSwarmManager()
-	if err != nil {
-		return err
-	}
-
-	if isManager {
-		trace("[swarm] already a swarm manager, no need to force.")
-		return nil
-	}
-
-	trace("[swarm] forcing the Docker daemon into swarm mode.")
-
-	status := client.SwarmLeave()
-	fmt.Println(strings.Join(status.Stdout, "\n"))
-
-	status = client.SwarmInit()
-	if status.Exit == 0 {
-		fmt.Println(status.Stdout[0])
-	}
-	return nil
-}
-
-// SwarmInit initialized a swarm
-// `docker swarm init`
-func (client *DockerClient) SwarmInit() cmd.Status {
-	return ExecCommand("docker", []string{"swarm", "init"}, client.config.EnvVars(), client.config.Debug)
-}
-
-// SwarmState reports on the current state of the Docker node.
-// It is used to determine if its necessary to initialize or join a swarm.
-func (client *DockerClient) SwarmState() cmd.Status {
-	return ExecCommand("docker", []string{"info", "--format", "{{.Swarm.LocalNodeState}}"}, nil, client.config.Debug)
-}
-
 // SwarmLeave forces the current Docker node to leave a swarm. This is only really good for troubleshooting.
 // `docker swarm leave --force`
-func (client *DockerClient) SwarmLeave() cmd.Status {
-	return ExecCommand("docker", []string{"swarm", "leave", "--force"}, client.config.EnvVars(), client.config.Debug)
-}
-
-// IsSwarmNode determines if the current Docker node is part of a swarm.
-func (client *DockerClient) IsSwarmNode() (bool, error) {
-	status := client.SwarmState()
-	if status.Error != nil {
-		return false, status.Error
-	}
-
-	return "active" == strings.Join(status.Stdout, ""), nil
-}
-
-// IsSwarmManager determines if this current node is a manager of a swarm.
-func (client *DockerClient) IsSwarmManager() (bool, error) {
-	if _, err := client.IsSwarmNode(); err != nil {
-		return false, err
-	}
-
-	status := ExecCommand("docker", []string{"node", "inspect", "self", "-f", "{{.ManagerStatus.Leader}}"}, nil, client.config.Debug)
-	if status.Error != nil {
-		return false, status.Error
-	}
-	return "true" == strings.Join(status.Stdout, ""), nil
+func (client *DockerClient) SwarmLeave() error {
+	return Run("docker", []string{"swarm", "leave", "--force"}, client.config.EnvVars(), client.config.Debug)
 }
