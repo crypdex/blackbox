82d9e6dcee0a81af5a94c00f148c32d4e301621a
David Michael <david.michael@gmail.co>
2019-07-31 22:09:35 -0400

    system service not enabled by default

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,12 +3,11 @@
 # This script is executed by the deb package post-install
 
 echo "[postinstall] Enabling the admin systemd service"
-
 deb-systemd-invoke enable blackbox-admin.service
 
-echo "[postinstall] Enabling the systemd service"
+#echo "[postinstall] Enabling the systemd service"
+#deb-systemd-invoke enable blackbox.service
 
-deb-systemd-invoke enable blackbox.service
 deb-systemd-helper daemon-reload
 
 echo "[postinstall] Starting the admin ..."
